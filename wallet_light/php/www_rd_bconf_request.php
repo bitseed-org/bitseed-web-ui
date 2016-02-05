02b26d310c766a804c44e8e80a40d0fef3da4667
Konn Danley <kdanley42@gmail.com>
2016-02-05 11:30:03 +0000

    Removed test software for checkbox changes

--- wallet_light/php/www_rd_bconf_request.php
+++ wallet_light/php/www_rd_bconf_request.php
@@ -2,11 +2,15 @@
    $max_peers = 125;
    $minrelaytxfee = .00001;
    $limitfreerelay = 10;
+
+   // ------------------------------------------------------------------
    // This function issues a load request for loading bitcoin.conf
    // values. It will only issue a request if a rd_bconf_flag
    // has a '0'.  This is to prevent a new load request from 
    // initiating a new read operation when the previous operation
    // is still incomplete.
+   // write a '1' to rd_bconf_flag to kick things off.
+   // ------------------------------------------------------------------
    $fh_flag = fopen ("/home/linaro/rd_bconf_flag", "r+") or die ("Unable to open /home/linaro/rd_bconf_flag");
    if ($fh_flag) {
        if (($line = fgets($fh_flag)) !== false) {
@@ -18,17 +22,16 @@
        }
     }
 
+    // ------------------------------------------------------------------
     // Now wait for the linaro side to fill the read mailbox and write a '2' to rd_bconf_flag
     // Once that occurs, you can unload the read mailbox, fill the text boxes 
     // in the control page, then write a '0' to rd_bconf_flag
-
+    // ------------------------------------------------------------------
     while (1) {
-         
         sleep(1);
 
         // Open file for read and writing - To detect 2 for your own action
         // and the ability to write '0' when your opertion is complete.
-	    // There is only one line in the flag files.
 	    rewind($fh_flag);
         $line = fgets($fh_flag);
 		$line = trim($line);
@@ -37,35 +40,48 @@
             break;
 		}
     }
-		    // echo "Made it here";
+    // ------------------------------------------------------------------
     // A '2' has been detected.  Read the read mailbox and place a '0' in 
     // rd_bconf_flag.
+    // ------------------------------------------------------------------
     rewind($fh_flag);
-	$kh = fopen ("/home/linaro/temp_log", "w");
     $jsondata = file_get_contents('/home/linaro/rd_bconf_mbox');
     $array_from_json = json_decode($jsondata, true);
 
-    // Place the values from the array into the textboxexes in the control page.
-    // $max_peers=$array_from_json['max_peers']; 
+    // ------------------------------------------------------------------
+    // Place the values from the array into the textboxes in the control page.
+    // ------------------------------------------------------------------
     $max_peers_conf=$array_from_json['max_peers']; 
-	// echo "max_peers_conf $max_peers_conf";
 	if ($max_peers_conf) {
         $max_peers = $max_peers_conf;
 	}
-	fputs($kh, $max_peers);
-	fputs($kh, $max_peers_conf);
     $minrelaytxfee_conf=$array_from_json['minrelaytxfee']; 
-	// echo "minrelaytxfee_conf  $minrelaytxfee_conf";
 	if ($minrelaytxfee_conf) {
         $minrelaytxfee = $minrelaytxfee_conf;
 	}  
-	fputs($kh, $minrelaytxfee);
     $limitfreerelay_conf=$array_from_json['limitfreerelay']; 
-	// echo "limitfreerelay_conf $limitfreerelay_conf";
 	if (($limitfreerelay_conf)) {
         $limitfreerelay = $limitfreerelay_conf;
 	}
-	fputs($kh, $limitfreerelay);
+
+   // Next, populate the checkboxes with the values from bitcoin.conf.
+   $disablewallet_checked = "";
+   $disablewallet_conf=$array_from_json['disablewallet']; 
+   if ($disablewallet_conf == 1) {
+       $disablewallet_checked = "checked";
+   }
+
+   $txindex_checked = "";
+   $txindex_conf=$array_from_json['txindex']; 
+   if ($txindex_conf == 1) {
+       $txindex_checked = "checked";
+   }
+
+   $konn_checked = "";
+   $konn_conf=$array_from_json['konn']; 
+   if ($konn_conf == 1) {
+       $konn_checked = "checked";
+   }
 
    // Write a '0' to rd_bconf_flag
    rewind ($fh_flag);
--- wallet_light/php/www_wr_bconf_mbox.php
+++ wallet_light/php/www_wr_bconf_mbox.php
@@ -1,33 +1,75 @@
 <?php
 $HOME = "/home/linaro";
+
+$full_chkbox_array = ['disablewallet', 'txindex', 'konn'];
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// EXTRACT VALUES FROM UI BCUPDATE FORM SUBMIT
+// ----------------------------------------------------------
+// Process numeric boxes - 
+// ----------------------------------------------------------
+// Note that checkboxes are processed with an array type.  
+// This method should be compatible with All input types.  
+// Investigate unified methods for all input 
+// forms here.
+// ----------------------------------------------------------
 $max_peers=$_POST["max_peers"];
 $minrelaytxfee=$_POST["minrelaytxfee"];
 $limitfreerelay=$_POST["limitfreerelay"];
-// echo "$max_peers";
-// echo "$minrelaytxfee";
-// echo "$limitfreerelay";
 
-// Create an initial array of default parameters
-$params_default = array(
-                "max_peers" => 125,
-                "minrelaytxfee" => 0.000010000,
-                "limitfreerelay" => 15
-                );
+// ----------------------------------------------------------
+// Process checkboxes
+// ----------------------------------------------------------
+//  Checkbox array
+$bitcoin_conf_chkbox = count($_POST['bitcoin_conf_chkbox']) ? $_POST['bitcoin_conf_chkbox'] : array();
+// echo $bitcoin_conf_chkbox;
+// echo count($bitcoin_conf_chkbox) ? implode(', ', $bitcoin_conf_chkbox) : 'Nothing ';
+// $bitcoin_conf_list = array_values($bitcoin_conf_chkbox);
+// ----------------------------------------------------------
+// The checkbox arrays come put in the form of indexed arrays.
+// To remain consistent with the way numeric boxes are handled,
+// swap the value and the key.
+// ----------------------------------------------------------
+// echo var_dump($bitcoin_conf_chkbox);
+$chkbox_values = array();
+for ($i = 0; $i < count($full_chkbox_array); $i++) { 
+	$cbox_item = $full_chkbox_array[$i];
+	if (in_array($cbox_item, $bitcoin_conf_chkbox)) {
+        $chkbox_values[$cbox_item] = "1";
+    } else {
+	    $chkbox_values[$cbox_item] = "0";
+    }
+}
+// echo var_dump($chkbox_values);
 
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// CREATE AN INITIAL ARRAY OF DEFAULT PARAMETERS
+// ----------------------------------------------------------
+$params_default = array( 
+                'max_peers' => 125,
+                'minrelaytxfee' => 0.000010000,
+                'limitfreerelay' => 15,
+                'disablewallet' => 1,
+				'txindex' => 1,
+			    'konn' => 0	
+				);
+
+// ----------------------------------------------------------
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // If there is a value in the form element then assign 
 // the parameter to th
-$params_new = array (
-    'max_peers' => $max_peers,
+// ----------------------------------------------------------
+// Numeric Boxes
+$params_new = [
+        'max_peers' => $max_peers,
         'minrelaytxfee' => $minrelaytxfee,
         'limitfreerelay' => $limitfreerelay
-        );
-
-// ----------------------------------------------------------------
-// Read in the bitcoin.conf file into the lines a normal array.
-// Remove left and right whitespace as well as blank lines.
-// ----------------------------------------------------------------
-// $fh = fopen ("bitcoin.conf", "r") or die ("Unable to open file!");
-// $fh = fopen ("$HOME/bitcoin.conf", "r") or die ("Unable to open file!");
+        ];
+// Add checkbox settings as elements in $params_new
+$params_new = array_merge($params_new, $chkbox_values);
+// echo var_dump($params_new);
 
 // ------------------------------------------------------
 //  For the parameters passed by the client,  take into 
@@ -43,17 +85,18 @@ foreach ($params_new as $key => $val) {
         $valid_lines[$key] = $params_default[$key];
     }
 }
-// print $valid_lines;
+// echo var_dump($valid_lines);
 // Create a php array and then convert to a json object
 // before writing to the mailbox.  minrelaytxfee, which 
 // requires a floating point, 8 significant digit format,
 // needs to be handled special.
 foreach ($params_new as $key => $val) {
     if ($key == 'minrelaytxfee') {
-        $valid_lines[$key] = number_format ($val, 10);
+        $valid_lines[$key] = number_format ($val, 8);
     }	
 }
 $json_object = json_encode($valid_lines);
+// echo var_dump($json_object);
 
 // Write the json object to the mailbox.
 $fh = fopen ("$HOME/wr_bconf_mbox", "w");
@@ -67,5 +110,5 @@ $fh = fopen ("/home/linaro/wr_bconf_flag", "w+");
 fclose ($fh);
 
 echo "Bitcoin configuration parameters have been updated.  Restarting bitcoind...Please wait 15 minutes to resume normal operation. ";
-// shell_exec ('echo "1" > /home/linaro/restartflag');
+shell_exec ('echo "1" > /home/linaro/restartflag'); // Comment this line out for testing purposes
 ?>
