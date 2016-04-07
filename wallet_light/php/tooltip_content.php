fbceed6d1bf917ed3a1da9ca904eb2958b4aa14d
Jay Feldis <jfeldis@bitseed.org>
2016-04-07 00:42:28 -0700

    Update tooltip_content.php

--- wallet_light/php/tooltip_content.php
+++ wallet_light/php/tooltip_content.php
@@ -3,13 +3,14 @@
 // --------------------------------------------------------------------------
 
 // Disable Wallet
-$disablewallet_tooltip = "Bitseed is not indended for storage of bitcoins. If you decide to enable the wallet capability of bi$
+$disablewallet_tooltip = "Bitseed is not indended for storage of bitcoins. If you decide to enable the wallet responsibe for making sure the wallet is properly backed-up and secure.";
 
 // Auto Updates
 $autoupdate_tooltip = "Auto Updates Help";
 
 // Tor Enable
-$listenonion_tooltip = "Bitcoin will relay blocks and transactions over both the IPv4 clear net and the Tor network. transacti$
+$listenonion_tooltip = "Bitcoin will relay blocks and transactions over both the IPv4 clear net and the Tor network. Transactions are not relayed privately, but the node will act as a bridge for bitcoin.";
+
 
 // Tor Only Mode
 $onlynet_tooltip = "When enabled, bitcoin core will operate exclusively on Tor network";
@@ -21,13 +22,13 @@ $upnp_tooltip = "Universal Plug and Play Help";
 $disablebackups_tooltip = "Frees up disk space by disabling the blockchain backup.";
 
 // Min Relay Tx Fee
-$minrelaytxfee_tooltip = "Min Relay Tx Fee Help";
+$minrelaytxfee_tooltip = "Transactions with a fee below this amount will not be relayed by this node.";
 
 // Daily Upload Limit
-$maxuploadtarget_tooltip = "Daily Upload Limit Help";
+$maxuploadtarget_tooltip = "Limits how much blockchain data this node will upload to other nodes each day";
 
 // Mempool Size Limit
-$maxmempool_tooltip = "Mempool Size Limit Help";
+$maxmempool_tooltip = "Sets the amount of RAM memory to be used for storing unconfirmed transations (the mempool)";
 
 // ------------------------------------------------------------
 // Form finished content to place into tooltip.  
