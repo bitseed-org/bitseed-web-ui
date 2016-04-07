9455c4facd8e2d94ff6ea4f431a0db6a7ba176ee
Jay Feldis <jfeldis@bitseed.org>
2016-04-07 00:21:18 -0700

    Add tooltip content

--- wallet_light/php/tooltip_content.php
+++ wallet_light/php/tooltip_content.php
@@ -1,27 +1,24 @@
-<?php
-
-// --------------------------------------------------------------------------
 //  TOOLTIP CONTENTi
 //  Replace text in quotes below for tooltip content.
 // --------------------------------------------------------------------------
 
 // Disable Wallet
-$disablewallet_tooltip = "Disable Wallet Help";
+$disablewallet_tooltip = "Bitseed is not indended for storage of bitcoins. If you decide to enable the wallet capability of bi$
 
 // Auto Updates
 $autoupdate_tooltip = "Auto Updates Help";
 
 // Tor Enable
-$listenonion_tooltip = "Tor Help";
+$listenonion_tooltip = "Bitcoin will relay blocks and transactions over both the IPv4 clear net and the Tor network. transacti$
 
 // Tor Only Mode
-$onlynet_tooltip = "Tor Only Mode Help";
+$onlynet_tooltip = "When enabled, bitcoin core will operate exclusively on Tor network";
 
 // Universal Plug and Play
 $upnp_tooltip = "Universal Plug and Play Help";
 
 // Disable Blockchain Backups
-$disablebackups_tooltip = "Disable Blockchain Backups Help";
+$disablebackups_tooltip = "Frees up disk space by disabling the blockchain backup.";
 
 // Min Relay Tx Fee
 $minrelaytxfee_tooltip = "Min Relay Tx Fee Help";
